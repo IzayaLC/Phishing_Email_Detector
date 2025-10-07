85a8f016d56445c7fe73c4a83ed2201946407ba9
IsaiahLugo <isaiahlugom@gmail.com>
2025-10-07 14:43:10 -0400

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import re
 
+
+    
 with open('sample.txt', 'r') as rf:
-    with open('sample_copy.txt', 'w') as wf:
-        for line in rf:
-            wf.write(line)
-    
-    
+    for line_number, line in enumerate(rf, start=1):
+        if re.search(r"\burgent\b", line, re.IGNORECASE):
+            print(f"Searched word found on line {line_number}")
+                
+
    
 
