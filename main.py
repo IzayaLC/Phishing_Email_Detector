f45c921d235f372d00f91de747ddbe216d187c08
IsaiahLugo <isaiahlugom@gmail.com>
2025-10-16 12:08:33 -0400

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import re
-
+from rules import phishing_keywords
 
     
-with open('sample.txt', 'r') as rf:
-    for line_number, line in enumerate(rf, start=1):
-        if re.search(r"\burgent\b", line, re.IGNORECASE):
-            print(f"Searched word found on line {line_number}")
+with open('sample.txt', 'r') as f:
+    text = f.read().lower()
                 
+matches = []
+for kw in phishing_keywords:
+    if kw.lower() in text:
+        matches.append(kw.lower())
+    
+    
+print('-' * 50, 'Suspicious Language Detected', '-' * 50)
+print(matches)
+        
 
    
 
