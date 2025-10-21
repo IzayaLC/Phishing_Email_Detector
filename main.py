3109d5d0fc3fc4d5dd20e6046932e0a965ad5aa0
IsaiahLugo <isaiahlugom@gmail.com>
2025-10-21 13:01:12 -0400

    README.md

--- main.py
+++ main.py
@@ -2,21 +2,42 @@ import re
 from rules import phishing_keywords
 
     
-with open('sample.txt', 'r') as f:
-    text = f.read().lower()
+def detect_suspicious_language(path, words):
+    with open(path, 'r') as f:
+        text = f.read().lower()
+    
+    matches = []
+    for kw in words:
+        if kw.lower() in text:
+            matches.append(kw.lower())
+            
+    return matches
+
+def detect_sender_reply_mismatch(path):
+    sender, reply_to = None, None
+    
+    with open(path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            
+            if line.lower().startswith('from:'):
+                sender = line.split('<')[-1].rstrip('>').lower()
+            elif line.lower().startswith('reply-to'):
+                reply_to = line.split('<')[-1].rstrip('>').lower()
                 
-matches = []
-for kw in phishing_keywords:
-    if kw.lower() in text:
-        matches.append(kw.lower())
+    if not sender or not reply_to:
+        return 'Sender domain or reply_to domain is missing'
     
+    if sender != reply_to:
+        return f'Mismatch detected - sender domain "{sender}" vs reply-to domain "{reply_to}"'
+    else:
+        return f'No mismatch - both from {sender}'
     
-if len(matches) > 0:
-    print('-' * 50, 'Suspicious Language Detected', '-' * 50)
-    print(matches)
-else:
-    print("No suspicious language found")
-        
 
-   
 
+
+if __name__ == "__main__":
+    path = 'sample.txt'
+    print('-' * 50, 'Email Report', '-' * 50)
+    print(detect_suspicious_language(path, phishing_keywords))
+    print(detect_sender_reply_mismatch(path))
